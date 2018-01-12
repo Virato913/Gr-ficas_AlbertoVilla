75226cafd465af75f6a6a0034589123c26e1541b
Alberto Villa <idv16c.jvilla@uartesdigitales.edu.mx>
2018-01-11 21:34:42 -0600

    *Modified .cpp file to add code which allows the user to print the vertex's coordinates at will with the Space Bar, or all at once with the Enter Key, as well as some general improvements

--- Gráficas_AlbertoVilla/Gráficas_AlbertoVilla/Gráficas_AlbertoVilla.cpp
+++ Gráficas_AlbertoVilla/Gráficas_AlbertoVilla/Gráficas_AlbertoVilla.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
@@ -28,38 +29,43 @@ int main()
 	vector<Coordinates*> MeshList;
 	vector<Coordinates*>::iterator it;
 	string sIn, sFileName;
-	char cChar;
-	int iVertexCount, iOption;
+	size_t iFound;
+	char cChar, cInput;
+	int iVertexCount;
+	bool bMeshFound;
 
 	do
 	{
-		cout << "Archivo a abrir: \n1.- CerdoNuevo\n2.- Drone\n3.- NuCroc\n4.- NuVenomJok\n5.- Pig\n6.- Piramid\n7.- Scene\n8.- Salir\n\n";
-		cin >> iOption;
-		MeshList.clear();
-		switch (iOption)
+		bMeshFound = false;
+		system("cls");
+		cout << "Archivo a abrir: \n1.- CerdoNuevo\n2.- Drone\n3.- NuCroc\n4.- NuVenomJok\n5.- Pig\n6.- Piramid\n7.- Scene\nCualquier otro numero.- Salir\n\n";
+		cInput = _getch();
+		//MeshList.clear();
+		switch (cInput)
 		{
-		case 1:
+		case '1':
 			sFileName = "CerdoNuevo";
 			break;
-		case 2:
+		case '2':
 			sFileName = "Drone";
 			break;
-		case 3:
+		case '3':
 			sFileName = "NuCroc";
 			break;
-		case 4:
+		case '4':
 			sFileName = "NuVenomJok";
 			break;
-		case 5:
+		case '5':
 			sFileName = "Pig";
 			break;
-		case 6:
+		case '6':
 			sFileName = "Piramid";
 			break;
-		case 7:
+		case '7':
 			sFileName = "Scene";
 			break;
 		default:
+			return -1;
 			break;
 		}
 
@@ -67,46 +73,64 @@ int main()
 
 		if (fFile.is_open())
 		{
-			cout << "Se abrio correctamente el archivo.\n";
+			system("cls");
+			cout << "Se abrio correctamente el archivo " << sFileName << ".\n";
 			system("pause");
-			while (!fFile.eof())
+			while (!bMeshFound)
 			{
 				getline(fFile, sIn);
-				size_t iFound = sIn.find("Mesh");
-				if (iFound == 1)
+				iFound = sIn.find(" Mesh ");
+				if (iFound == 0)
 				{
 					fFile >> iVertexCount;
 					fFile >> cChar;
-					for (int i = 0;i < iVertexCount;i++)
-					{
-						Coordinates* pivot = new Coordinates;
-						fFile >> pivot->x;
-						fFile >> cChar;
-						fFile >> pivot->y;
-						fFile >> cChar;
-						fFile >> pivot->z;
-						fFile >> cChar;
-						fFile >> cChar;
-						MeshList.push_back(pivot);
-					}
-					for (int i = 0;i < iVertexCount;i++)
-					{
-						/*cout << MeshList[i];*/
-						cout << "(" << MeshList[i]->x << ", " << MeshList[i]->y << ", " << MeshList[i]->z << ")\n";
-					}
-					break;
+					bMeshFound = true;
 				}
+			}
+			if (bMeshFound)
+			{
+				for (int i = 0; i < iVertexCount; i++)
+				{
+					Coordinates* pivot = new Coordinates;
+					fFile >> pivot->x;
+					fFile >> cChar;
+					fFile >> pivot->y;
+					fFile >> cChar;
+					fFile >> pivot->z;
+					fFile >> cChar;
+					fFile >> cChar;
+					MeshList.push_back(pivot);
+				}
+				system("cls");
+				cout << "Presione la barra espaciadora para mostrar una vertice a la vez, o la tecla Enter para imprimir todo.\n";
+				cInput = _getch();
+				for (int i = 0; i < iVertexCount; i++)
+				{
+					/*cout << MeshList[i];*/
+					while (cInput != 32 && cInput != 13)
+					{
+						cout << "Presione la barra espaciadora para mostrar una vertice a la vez, o la tecla Enter para imprimir todo.\n";
+						cInput = _getch();
+					}
+					cout << "(" << MeshList[i]->x << ", " << MeshList[i]->y << ", " << MeshList[i]->z << ")\n";
+					if (cInput == 32)
+					{
+						cInput = _getch();
+					}
+				}
+				system("pause");
+				bMeshFound = false;
+				fFile.close();
+				MeshList.clear();
 			}
 		}
 		else
 		{
-			cout << "No se encontro el archivo.\n";
+			cout << "No se encontro el archivo " << sIn << ".\n";
 			system("pause");
 		}
 
-	} while (iOption != 8);
-
-	fFile.close();
+	} while (cInput != '8');
 	return 0;
 }
 
